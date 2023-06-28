1616176e0d33d49c9f29a1f8b4e6acbff7e8f71f
TheMadman <stretenskijdanila@gmail.com>
2023-06-28 22:19:35 +0500

    Начало
    Создал базовое Spring приложение

---
+++ recreationAssistant/src/test/java/ru/recreation/recreationassistant/RecreationAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.recreation.recreationassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecreationAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
