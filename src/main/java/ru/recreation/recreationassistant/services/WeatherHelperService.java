30f1ac5304bd3dd26d4ca2a7a4eb6dc19c13f27c
BenSerg <bendrysevsergej@gmail.com>
2023-07-08 16:02:46 +0300

    merge methods

--- src/main/java/ru/recreation/recreationassistant/services/WeatherHelperService.java
+++ src/main/java/ru/recreation/recreationassistant/services/WeatherHelperService.java
@@ -1,14 +1,8 @@
 package ru.recreation.recreationassistant.services;
 
-import org.springframework.http.ResponseEntity;
 import ru.recreation.recreationassistant.models.City;
-import ru.recreation.recreationassistant.models.Forecast;
 
 public interface WeatherHelperService
 {
-
-    ResponseEntity<String> makeRequest(City city);
-    Forecast getForecast(ResponseEntity<String> json);
-    String getRecommendation(Forecast weather);
-
+    String makeRequest(City city);
 }
