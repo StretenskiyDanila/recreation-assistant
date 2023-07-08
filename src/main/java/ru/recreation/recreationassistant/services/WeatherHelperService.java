dcc1762bb43ffd8079dc826b2b5fd1d0699a8799
BenSerg <bendrysevsergej@gmail.com>
2023-07-08 15:33:23 +0300

    Добавил рекомендации

--- src/main/java/ru/recreation/recreationassistant/services/WeatherHelperService.java
+++ src/main/java/ru/recreation/recreationassistant/services/WeatherHelperService.java
@@ -4,13 +4,11 @@ import org.springframework.http.ResponseEntity;
 import ru.recreation.recreationassistant.models.City;
 import ru.recreation.recreationassistant.models.Forecast;
 
-import java.util.List;
-
 public interface WeatherHelperService
 {
 
     ResponseEntity<String> makeRequest(City city);
     Forecast getForecast(ResponseEntity<String> json);
-    String getRecomendation(Forecast weather);
+    String getRecommendation(Forecast weather);
 
 }
