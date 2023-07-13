92eeaf4d0fbb8c3ddda8cd039a78ecc77f0995f5
serg <benserg>
2023-07-13 18:36:43 +0300

    Исправил названия и рекомендации

--- src/main/java/ru/recreation/recreationassistant/services/impl/WeatherHelperServiceImpl.java
+++ src/main/java/ru/recreation/recreationassistant/services/impl/WeatherHelperServiceImpl.java
@@ -2,10 +2,10 @@ package ru.recreation.recreationassistant.services.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import ru.recreation.recreationassistant.configurations.WeatherApiConfig;
 import ru.recreation.recreationassistant.models.City;
 import ru.recreation.recreationassistant.models.FactWeather;
 import ru.recreation.recreationassistant.models.Recommendation;
@@ -18,11 +18,10 @@ public class WeatherHelperServiceImpl implements WeatherHelperService {
     public String getRecommendation(City city) throws JsonProcessingException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        String key = apiConfig.getApiKey(), name = apiConfig.getApiKeyName();
-        headers.add(name, key);
+        headers.add(API_KEY_NAME, API_KEY);
         HttpEntity<String> request = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
-        String sb = apiConfig.getUrlApi() + "?lat=" + city.getLatitude() +
+        String sb = API_URL + "?lat=" + city.getLatitude() +
                 "&lon=" + city.getLongitude() +
                 "&extra=true";
         return getStringRecommendation(getForecast(restTemplate.exchange(sb, HttpMethod.GET, request, String.class)));
@@ -39,12 +38,17 @@ public class WeatherHelperServiceImpl implements WeatherHelperService {
         if (weather.feels_like > 17) {
             result.append(Recommendation.WARM_INFO).append(Recommendation.TEMP_FEELS).append(weather.feels_like).append("°C. ");
         } else {
-            result.append(Recommendation.COLD_WARNING).append(Recommendation.TEMP_FEELS).append(weather.feels_like).append("°C. ");
+            result.append(Recommendation.COLD_WARNING).append(Recommendation.TEMP_FEELS).append(weather.feels_like).append(" °C. ");
         }
         if (weather.condition.equals("rain")) {
             result.append(Recommendation.RAIN_WARNING);
         }
         return result.toString();
     }
-    private WeatherApiConfig apiConfig;
+    private static final String API_URL = "https://api.weather.yandex.ru/v2/forecast";
+    @Value("${weather_api_key}")
+    private String API_KEY;
+    @Value("${weather_api_key_name}")
+    private String API_KEY_NAME;
+
 }
